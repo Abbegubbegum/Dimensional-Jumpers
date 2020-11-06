d8984c156f6f5cc2b490e0d60128760c36f7f979
Albin <albin.nojback@gmail.com>
2020-11-06 13:32:33 +0100

    Introscreen Done

--- Dimensional Jumper/Program.cs
+++ Dimensional Jumper/Program.cs
@@ -2,22 +2,100 @@
 using System.Numerics;
 using Raylib_cs;
 
-
 namespace Dimensional_Jumper
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Raylib.InitWindow(1920, 1000, "Dimensional Jumper");
+            //Define global variables
+            const int windowX = 1920;
+            const int windowY = 1000;
+            string gameState = "intro";
+
+            //Define intro variables
+            int introMenuIndex = 0;
+            Color[] menuColors = { Color.BLACK, Color.GRAY, Color.GRAY, Color.GRAY };
+
+
+            Raylib.InitWindow(windowX, windowY, "Dimensional Jumper");
 
             while (!Raylib.WindowShouldClose())
             {
-                Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.WHITE);
+                if (gameState == "intro")
+                {
+                    //Logic Introscreen
+                    //Menu selecting and coloring
+                    if (Raylib.IsKeyPressed(KeyboardKey.KEY_S))
+                    {
+                        menuColors[introMenuIndex] = Color.GRAY;
+                        if (introMenuIndex == menuColors.Length - 1)
+                        {
+                            introMenuIndex = 0;
+                        }
+                        else
+                        {
+                            introMenuIndex++;
+                        }
+                        menuColors[introMenuIndex] = Color.BLACK;
+                    }
+                    else if (Raylib.IsKeyPressed(KeyboardKey.KEY_W))
+                    {
+                        menuColors[introMenuIndex] = Color.GRAY;
+                        if (introMenuIndex == 0)
+                        {
+                            introMenuIndex = menuColors.Length - 1;
+                        }
+                        else
+                        {
+                            introMenuIndex--;
+                        }
+                        menuColors[introMenuIndex] = Color.BLACK;
+                    }
+                    //Menu confirming
+                    else if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+                    {
+                        switch (introMenuIndex)
+                        {
+                            case 0:
+                                gameState = "game";
+                                break;
 
-                Raylib.EndDrawing();
+                            case 1:
+                                gameState = "controls";
+                                break;
+                            case 3:
+                                Raylib.CloseWindow();
+                                break;
+                        }
+                    }
+
+
+
+                    //Drawing Introscreen
+                    Raylib.BeginDrawing();
+                    Raylib.ClearBackground(Color.PURPLE);
+
+                    //Title
+                    Raylib.DrawText("Dimensional", 50, 100, 100, Color.BLACK);
+                    Raylib.DrawText("Jumper", 50, 200, 100, Color.BLACK);
+
+                    //The menu
+                    Raylib.DrawText("Start", 50, 400, 64, menuColors[0]);
+                    Raylib.DrawText("Controls", 50, 460, 64, menuColors[1]);
+                    Raylib.DrawText("Level Select", 50, 520, 64, menuColors[2]);
+                    Raylib.DrawText("Exit", 50, 580, 64, menuColors[3]);
+
+                    Raylib.EndDrawing();
+                }
+                else
+                {
+                    Raylib.BeginDrawing();
+                    Raylib.ClearBackground(Color.WHITE);
+                    Raylib.EndDrawing();
+                }
             }
         }
     }
 }
+
