1b8731945bd9031bf00c65ceee657a9a66a5508c
Albin <albin.nojback@gmail.com>
2020-11-06 12:26:44 +0100

    Initial Commit

---
+++ Dimensional Jumper/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+using Raylib_cs;
+
+
+namespace Dimensional_Jumper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(1920, 1000, "Dimensional Jumper");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
