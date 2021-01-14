1a796160f8b5ae124541d121a1516a1bf86c86c0
Albin <albin.nojback@gmail.com>
2021-01-14 15:17:55 +0100

    Start of Level 5

--- Dimensional Jumper/Program.cs
+++ Dimensional Jumper/Program.cs
@@ -23,12 +23,15 @@ namespace Dimensional_Jumper
             //Intro variables
             int introMenuIndex = 0;
             Color[] menuColors = { Color.BLACK, Color.GRAY, Color.GRAY, Color.GRAY };
+            int levelMenuIndex = 0;
+            Color[] levelMenuColors = { Color.BLACK, Color.GRAY, Color.GRAY, Color.GRAY, Color.GRAY };
+
             string musicText = "Music ON";
 
             //Game variables
             Rectangle goalRec = new Rectangle(1700, 700, 100, 100);
-            Player p = new Player(new Rectangle(100, 50, 50, 100), Color.RED);
-            Platform[] platforms = new Platform[25];
+            Player p = new Player();
+            Platform[] platforms = new Platform[30];
             //Level one platforms
             platforms[0] = new Platform(new Rectangle(0, 200, windowX, 20), 1);
             platforms[1] = new Platform(new Rectangle(0, 400, windowX, 20), 2);
@@ -56,9 +59,17 @@ namespace Dimensional_Jumper
             platforms[20] = new Platform(new Rectangle(1400, 200, 400, 20), 1);
             platforms[21] = new Platform(new Rectangle(1780, 200, 20, 400), 1);
             platforms[22] = new Platform(new Rectangle(1400, 600, 400, 20), 1);
+            //Level five platforms
+            platforms[23] = new Platform(new Rectangle(200, 400, 200, 800), 1);
+            platforms[24] = new Platform(new Rectangle(200, 600, 400, 600), 1);
+            platforms[25] = new Platform(new Rectangle(200, 800, 600, 400), 1);
+            platforms[26] = new Platform(new Rectangle(200, 1000, 800, 200), 1);
+            platforms[27] = new Platform(new Rectangle(100, 960, 150, 20), 1);
+            platforms[28] = new Platform(new Rectangle(100, 980, 800, 20), 2);
 
+            Color gameBackground = Color.SKYBLUE;
+            int[] platformStartIndexes = { 0, 4, 6, 11, 23, 29 };
             int dimension = 1;
-            int[] platformStartIndexes = { 0, 4, 6, 11, 23 };
             int frameCount = 0;
             int dimensionFlipFrame = -200;
             int deathCount = 0;
@@ -115,13 +126,17 @@ namespace Dimensional_Jumper
                         {
                             case 0:
                                 gamestate = "game";
-                                dimension = 1;
                                 for (int i = platformStartIndexes[level - 1]; i < platformStartIndexes[level]; i++)
                                 {
                                     platforms[i].checkDimension(dimension);
                                 }
-                                p.checkDimension(dimension);
+                                gameBackground = Color.SKYBLUE;
 
+
+                                break;
+
+                            case 1:
+                                gamestate = "levelSelect";
                                 break;
 
                             case 2:
@@ -174,6 +189,128 @@ namespace Dimensional_Jumper
 
                     Raylib.EndDrawing();
                 }
+                else if (gamestate == "levelSelect")
+                {
+
+                    if (Raylib.IsKeyPressed(KeyboardKey.KEY_S))
+                    {
+                        levelMenuColors[levelMenuIndex] = Color.GRAY;
+                        if (levelMenuIndex == levelMenuColors.Length - 1)
+                        {
+                            levelMenuIndex = 0;
+                        }
+                        else
+                        {
+                            levelMenuIndex++;
+                        }
+                        levelMenuColors[levelMenuIndex] = Color.BLACK;
+
+                    }
+                    else if (Raylib.IsKeyPressed(KeyboardKey.KEY_W))
+                    {
+                        levelMenuColors[levelMenuIndex] = Color.GRAY;
+                        if (levelMenuIndex == 0)
+                        {
+                            levelMenuIndex = levelMenuColors.Length - 1;
+                        }
+                        else
+                        {
+                            levelMenuIndex--;
+                        }
+                        levelMenuColors[levelMenuIndex] = Color.BLACK;
+
+                    }
+                    else if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+                    {
+                        switch (levelMenuIndex)
+                        {
+                            case 0:
+                                gamestate = "game";
+                                level = 1;
+                                dimension = 1;
+                                p.startX = 100;
+                                p.startY = 50;
+                                p.rec.x = p.startX;
+                                p.rec.y = p.startY;
+                                p.accY = 0;
+                                goalRec.x = 1700;
+                                goalRec.y = 700;
+                                for (int i = platformStartIndexes[level - 1]; i < platformStartIndexes[level]; i++)
+                                {
+                                    platforms[i].checkDimension(dimension);
+                                }
+                                gameBackground = Color.SKYBLUE;
+                                break;
+
+                            case 1:
+                                gamestate = "game";
+                                level = 2;
+                                p.startX = 100;
+                                p.startY = 100;
+                                p.rec.x = p.startX;
+                                p.rec.y = p.startY;
+                                p.accY = 0;
+                                goalRec.x = 1750;
+                                goalRec.y = 500;
+                                dimension = 1;
+                                for (int i = platformStartIndexes[level - 1]; i < platformStartIndexes[level]; i++)
+                                {
+                                    platforms[i].checkDimension(dimension);
+                                }
+                                gameBackground = Color.SKYBLUE;
+                                break;
+
+                            case 2:
+                                gamestate = "game";
+                                level = 3;
+                                p.startX = 120;
+                                p.startY = 600;
+                                p.rec.x = p.startX;
+                                p.rec.y = p.startY;
+                                p.accY = 0;
+                                goalRec.x = 1750;
+                                goalRec.y = 800;
+                                dimension = 1;
+                                for (int i = platformStartIndexes[level - 1]; i < platformStartIndexes[level]; i++)
+                                {
+                                    platforms[i].checkDimension(dimension);
+                                }
+                                gameBackground = Color.SKYBLUE;
+                                break;
+
+                            case 3:
+                                gamestate = "game";
+                                level = 4;
+                                p.startX = 400;
+                                p.startY = 400;
+                                p.rec.x = p.startX;
+                                p.rec.y = p.startY;
+                                p.accY = 0;
+                                goalRec.x = 1550;
+                                goalRec.y = 700;
+                                dimension = 1;
+                                for (int i = platformStartIndexes[level - 1]; i < platformStartIndexes[level]; i++)
+                                {
+                                    platforms[i].checkDimension(dimension);
+                                }
+                                gameBackground = Color.SKYBLUE;
+                                break;
+                            case 4:
+                                gamestate = "intro";
+                                break;
+                        }
+                    }
+                    Raylib.BeginDrawing();
+                    Raylib.ClearBackground(Color.PURPLE);
+
+                    Raylib.DrawText("Level 1", 50, 400, 64, levelMenuColors[0]);
+                    Raylib.DrawText("Level 2", 50, 460, 64, levelMenuColors[1]);
+                    Raylib.DrawText("Level 3", 50, 520, 64, levelMenuColors[2]);
+                    Raylib.DrawText("Level 4", 50, 580, 64, levelMenuColors[3]);
+                    Raylib.DrawText("Back", 50, 640, 64, levelMenuColors[4]);
+
+                    Raylib.EndDrawing();
+                }
                 else if (gamestate == "game")
                 {
                     //Logic for the game
@@ -194,7 +331,7 @@ namespace Dimensional_Jumper
 
 
 
-                    //If you switched dimensions last frame and now collide, then you respawn
+                    //If you switched dimensions last frame and now collide, then you die
                     if (dimensionFlipFrame + 1 == frameCount)
                     {
                         for (int i = platformStartIndexes[level - 1]; i < platformStartIndexes[level]; i++)
@@ -240,7 +377,8 @@ namespace Dimensional_Jumper
                             {
                                 platforms[i].checkDimension(dimension);
                             }
-                            p.checkDimension(dimension);
+                            gameBackground = Color.SKYBLUE;
+
                             level++;
                             p.startX = 100;
                             p.startY = 100;
@@ -258,7 +396,8 @@ namespace Dimensional_Jumper
                             {
                                 platforms[i].checkDimension(dimension);
                             }
-                            p.checkDimension(dimension);
+                            gameBackground = Color.SKYBLUE;
+
 
                             p.startX = 120;
                             p.startY = 600;
@@ -276,7 +415,8 @@ namespace Dimensional_Jumper
                             {
                                 platforms[i].checkDimension(dimension);
                             }
-                            p.checkDimension(dimension);
+                            gameBackground = Color.SKYBLUE;
+
                             p.startX = 400;
                             p.startY = 400;
                             p.rec.x = p.startX;
@@ -286,6 +426,23 @@ namespace Dimensional_Jumper
                             goalRec.y = 700;
                         }
                         else if (level == 4)
+                        {
+                            level++;
+                            dimension = 1;
+                            for (int i = platformStartIndexes[level - 1]; i < platformStartIndexes[level]; i++)
+                            {
+                                platforms[i].checkDimension(dimension);
+                            }
+                            gameBackground = Color.SKYBLUE;
+                            p.startX = 125;
+                            p.startY = 800;
+                            p.rec.x = p.startX;
+                            p.rec.y = p.startY;
+                            p.accY = 0;
+                            goalRec.x = 200;
+                            goalRec.y = 100;
+                        }
+                        else if (level == 5)
                         {
                             gamestate = "finish";
                         }
@@ -301,17 +458,20 @@ namespace Dimensional_Jumper
                     {
                         if (dimension == 1)
                         {
+                            gameBackground = new Color(15, 15, 15, 255);
                             dimension = 2;
                         }
                         else
                         {
+                            gameBackground = Color.SKYBLUE;
                             dimension = 1;
                         }
                         for (int i = platformStartIndexes[level - 1]; i < platformStartIndexes[level]; i++)
                         {
                             platforms[i].checkDimension(dimension);
                         }
-                        p.checkDimension(dimension);
+
+
 
                         dimensionFlipFrame = frameCount;
                     }
@@ -321,7 +481,7 @@ namespace Dimensional_Jumper
 
                     //Drawing level 
                     Raylib.BeginDrawing();
-                    Raylib.ClearBackground(Color.BLACK);
+                    Raylib.ClearBackground(gameBackground);
                     Raylib.DrawText("Death Count: " + deathCount, 25, 25, 64, Color.WHITE);
                     Raylib.DrawText("Time: " + minuteCount + ":" + secondCount, 1500, 25, 64, Color.WHITE);
 
@@ -334,7 +494,7 @@ namespace Dimensional_Jumper
                     }
 
                     //Draw the goal
-                    Raylib.DrawRectangleRec(goalRec, Color.GREEN);
+                    Raylib.DrawRectangleRec(goalRec, Color.DARKGREEN);
 
                     //Draw the player
                     p.Draw();
@@ -348,6 +508,7 @@ namespace Dimensional_Jumper
                     {
                         gamestate = "intro";
                         level = 1;
+                        dimension = 1;
                         p.startX = 100;
                         p.startY = 50;
                         p.rec.x = p.startX;
@@ -394,8 +555,8 @@ namespace Dimensional_Jumper
     //Making a player class with a rectangle and a color
     class Player
     {
-        public Rectangle rec;
-        public Color c;
+        public Rectangle rec = new Rectangle(100, 50, 50, 100);
+        public Color c = Color.RED;
         public int xspeed = 15;
 
         public float oldX;
@@ -409,13 +570,6 @@ namespace Dimensional_Jumper
 
         public int g = 3;
         public int accY = 0;
-
-
-        public Player(Rectangle r, Color p)
-        {
-            this.rec = r;
-            this.c = p;
-        }
 
         public void Update()
         {
@@ -491,17 +645,6 @@ namespace Dimensional_Jumper
             Raylib.DrawRectangleRec(this.rec, this.c);
         }
 
-        public void checkDimension(int dimension)
-        {
-            if (dimension == 1)
-            {
-                this.c = Color.RED;
-            }
-            else
-            {
-                this.c = Color.WHITE;
-            }
-        }
     }
 
 
@@ -520,14 +663,7 @@ namespace Dimensional_Jumper
         {
             this.rec = r;
             this.dimensionActive = d;
-            if (this.dimensionActive == 1)
-            {
-                this.active = true;
-            }
-            else
-            {
-                this.active = false;
-            }
+            this.checkDimension(1);
         }
 
         public void Draw()
